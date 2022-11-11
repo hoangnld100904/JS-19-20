94867b8484fb35ea8ae5e9fdaae6b4e87c825f04
hoangnld100904 <114505869+hoangnld100904@users.noreply.github.com>
2022-11-11 15:30:24 +0700

    Hoan thanh chuc nang cap nhat

--- QuanLyNhanVien/js/baitap/Danhsachnhanvien.js
+++ QuanLyNhanVien/js/baitap/Danhsachnhanvien.js
@@ -5,4 +5,22 @@ function DanhSachNhanVien(){
     this.themNV= function(nv){
         this.mangNV.push(nv)
     }
+    this.timViTri=function(user){
+        var viTri=-1;
+        viTri= this.mangNV.findIndex(function(nv){
+            return nv.user== user;
+        })
+        return viTri;
+    }
+    this.xoaNhanVien=function(index){
+        dsnv.mangNV.splice(index,1)
+    }
+    this.capNhatNhanVien= function(nhanVienMoi){
+        var viTri = this.timViTri(nhanVienMoi.user);
+        console.log(viTri)
+        if(viTri > -1){
+            //tìm thấy
+            dsnv.mangNV[viTri] = nhanVienMoi
+        }
+    }
 }
--- QuanLyNhanVien/js/baitap/main.js
+++ QuanLyNhanVien/js/baitap/main.js
@@ -9,6 +9,30 @@ var dsnv = new DanhSachNhanVien();
 function GetELE(id){
     return document.getElementById(id)
 }
+function setLocalStorage() {
+    //?lưu mảng SV xuống localStorage
+    //? dữ liệu của localStorage hay BE là dữ liệu JSON
+    //?input: array (mangSV) => JSON
+
+    //localStorage, JSON: đối tượng cung cấp sẵn của JS
+    //stringify : array, object => JSON
+    //setItem("tên local", JSON)
+    localStorage.setItem("DSNV", JSON.stringify(dsnv.mangNV));
+}
+function getLocalStorage() {
+    //? 1 .getItem("tên local") => trả kết là JSON
+    //? 2. parse: JSON => mảng
+    //? 3. lưu vào dssv.mangSV
+
+    // mangSV = [] => push, for
+    //!mangSV  = null/undefine => không dùng được push, for
+    if (localStorage.getItem("DSNV") != null) {
+        dsnv.mangNV = JSON.parse(localStorage.getItem("DSNV"));
+        hienThiTable(dsnv.mangNV);
+    }
+
+}
+getLocalStorage();
 function themNhanVien(){
     //Lấy thông tin từ form
     var user= GetELE('tknv').value;
@@ -24,6 +48,7 @@ function themNhanVien(){
     nv.tongLuong()
     dsnv.themNV(nv);  
     hienThiTable(dsnv.mangNV)
+    setLocalStorage()
 }
 function hienThiTable(array){
     var content="" //String chứa HTML content
@@ -45,18 +70,26 @@ function hienThiTable(array){
     GetELE("tableDanhSach").innerHTML= content;
 }
 //Find user
-function timNhanVien(array, user){
-    var nv = array.find(function(nv){
-        return nv.user == user
-    })
-    return nv
-}
-//Delete user
-function xoaNhanVien(index){
-    dsnv.mangNV.splice(index,1)
-    hienThiTable(dsnv.mangNV)
+function timNhanVien(){
+    
 }
 //Update user
+function capNhatNhanVien(){
+    var user= GetELE('tknv').value;
+    var name= GetELE('name').value;
+    var email=GetELE('email').value;
+    var password= GetELE('password').value;
+    var date= GetELE('datepicker').value;
+    var basicSalary= Number(GetELE('luongCB').value);
+    var position= GetELE('chucvu').value;
+    var workTime= Number(GetELE('gioLam').value);
+    var nvMoi = new NhanVien(user, name, email, password, date, basicSalary, position, workTime);
+    nvMoi.xepLoai();
+    nvMoi.tongLuong();
+    dsnv.capNhatNhanVien(nvMoi);
+    setLocalStorage();
+    getLocalStorage();
+}
 //Sort by ranking
 function locTheoXepLoai(){ 
     var rank= GetELE('searchName').value;
@@ -68,4 +101,11 @@ function locTheoXepLoai(){
         return nv.rank == rank;
     })
     hienThiTable(output)
+}
+//Delete user
+function xoaNhanVien(index){
+    dsnv.xoaNhanVien(index)
+    // hienThiTable(dsnv.mangNV)
+    setLocalStorage()
+    getLocalStorage()
 }
